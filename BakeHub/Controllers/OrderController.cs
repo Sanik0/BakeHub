67337e41419d1fbccd72a02edad1ce4e2b7548ac
Sanik0 <sanico.ronan.macugay@gmail.com>
2025-12-03 12:10:21 +0800

    added orders page

---
+++ BakeHub/Controllers/OrderController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BakeHub.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult Orders()
+        {
+            return View();
+        }
+
+        // GET: editOrder
+
+        public ActionResult EditOrder()
+        {
+            return View();
+        }
+
+        // GET: addOrder
+
+        public ActionResult AddOrder()
+        {
+            return View();
+        }
+    }
+}
